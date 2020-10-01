5a23eb300fd6139f71ebd5b47db1bda0371eeb3b
GabrielZilmar <gabrielzilmar@hotmail.com>
2020-10-01 16:10:57 -0300

    [+] store user

--- server/src/Repository/user.js
+++ server/src/Repository/user.js
@@ -1,7 +1,17 @@
 import connection from "../database/connection.js";
 
 const repository = {
-	store: async (req, res) => {},
+	store: async (req, res) => {
+		await connection("users")
+			.insert(req)
+			.catch((err) => {
+				return err.constraint.includes("email_unique")
+					? res.status(400).json({
+							Errors: ["The E-mail need to be unique."],
+					  })
+					: res.status(501).json(err);
+			});
+	},
 };
 
 export default repository;
--- server/src/database/migrations/20200930160810_users.js
+++ server/src/database/migrations/20200930160810_users.js
@@ -2,7 +2,7 @@ exports.up = function (knex) {
 	return knex.schema.createTable("users", function (table) {
 		table.uuid("id").primary();
 		table.string("name").notNullable();
-		table.string("email").notNullable();
+		table.string("email").notNullable().unique();
 		table.string("password").notNullable();
 	});
 };
