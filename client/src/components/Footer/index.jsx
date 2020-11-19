0d0a732276f2c377fcab460502e984a3f2118346
Gabriel Zilmar <51098008+GabrielZilmar@users.noreply.github.com>
2020-11-18 23:18:52 -0300

    Feature/footer (#6)
    * feat: finish footer
    
    * feat: finish landing page

---
+++ client/src/components/Footer/index.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+import FooterStyle from './styles';
+
+const Footer = () => (
+  <FooterStyle>
+    © 1908 Gabriel Zilmar. All Ridhts Reserved.
+  </FooterStyle>
+);
+
+export default Footer;
