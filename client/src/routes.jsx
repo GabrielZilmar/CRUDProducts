a329b8cbfa196615b8b5b6e84cc3206f876fb714
GabrielZilmar <gabrielzilmar@hotmail.com>
2020-11-21 01:03:41 -0300

    feat: init routes

--- client/src/routes.jsx
+++ client/src/routes.jsx
@@ -2,4 +2,16 @@ import React from 'react';
 
 import { BrowserRouter, Switch, Route } from 'react-router-dom';
 
+import LandingPage from './pages/Landing';
 import Login from './pages/Login';
+
+const Routes = () => (
+  <BrowserRouter>
+    <Switch>
+      <Route path="/" exact component={LandingPage} />
+      <Route path="/login" exact component={Login} />
+    </Switch>
+  </BrowserRouter>
+);
+
+export default Routes;
